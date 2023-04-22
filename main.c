af6181d8077d717346cdd53b20466d04a987c043
lamekino <zoey.j.mcbride@gmail.com>
2023-04-21 21:49:38 -0400

    get_new_filename uses a mask, started work on prefix

--- main.c
+++ main.c
@@ -23,40 +23,60 @@
  * directory as `filename`
  */
 void get_new_filename(char *buffer,
-                     size_t len,
+                     size_t max_len,
                      const char *filename,
+                     const char *prefix,
                      const struct stat *statinfo) {
-    int duplicate_counter = 0;
+    enum RenameProperties {
+        HAS_EXTENSION = 0x1,
+        HAS_DUPLICATES = 0x10,
+        HAS_PREFIX = 0x100,
+        HAS_EXTENSION_DUP_PREFIX = 0x111
+    };
+    unsigned int properties = 0;
+
+    int num_duplicates = 0;
     int file_exists = 0;
 
     const time_t creation_time = statinfo->st_ctime;
     const char *extension = strrchr(filename, '.');
 
-    while (duplicate_counter == 0 || file_exists) {
-        if (extension == NULL) {
-            if (duplicate_counter > 0) {
-                snprintf(buffer, len, "%ld.%d", creation_time, duplicate_counter);
-            }
-            else {
-                snprintf(buffer, len, "%ld", creation_time);
-            }
-        }
-        else {
-            if (duplicate_counter > 0) {
-                snprintf(buffer, len, "%ld.%d%s", creation_time,
-                        duplicate_counter, extension);
-            }
-            else {
-                snprintf(buffer, len, "%ld%s", creation_time, extension);
-            }
+    properties |= extension != NULL;
+    properties |= (prefix != NULL) << 2;
+
+    while (file_exists || num_duplicates == 0) {
+        switch (properties) {
+        case HAS_EXTENSION:
+            snprintf(buffer, max_len,
+                    "%ld%s", creation_time, extension);
+            break;
+        case HAS_DUPLICATES:
+            snprintf(buffer, max_len,
+                    "%ld.%d", creation_time, num_duplicates);
+            break;
+        case HAS_PREFIX:
+            UNIMPLEMENTED;
+        case HAS_EXTENSION | HAS_DUPLICATES:
+            snprintf(buffer, max_len,
+                    "%ld.%d%s", creation_time, num_duplicates, extension);
+            break;
+        case HAS_EXTENSION | HAS_PREFIX:
+            UNIMPLEMENTED;
+        case HAS_EXTENSION_DUP_PREFIX:
+            UNIMPLEMENTED;
+        default: /* no extension + no duplicates */
+            snprintf(buffer, max_len,
+                    "%ld", creation_time);
+            break;
         }
 
         file_exists = access(buffer, F_OK) == 0;
-        duplicate_counter++;
+        num_duplicates++;
+        properties |= HAS_DUPLICATES;
     }
 }
 
-void process_directory(DIR *dir, size_t max_fname_len) {
+void process_directory(const char *prefix, DIR *dir, size_t max_fname_len) {
     struct dirent *dir_entry = NULL;
     char confirm[64];
     while ((dir_entry = readdir(dir))) {
@@ -77,8 +97,14 @@ void process_directory(DIR *dir, size_t max_fname_len) {
            "could not allocate memory for filename."
         );
 
-        get_new_filename(new_filename, max_fname_len, old_filename, &statinfo);
+        get_new_filename(new_filename, max_fname_len, prefix, old_filename,
+                &statinfo);
+
         printf("renaming '%s' -> '%s'\n", old_filename, new_filename);
+
+        /*
+         * FIXME: memory leak if this fails
+         */
         EXIT_WHEN(rename(old_filename, new_filename) != 0,
                 "could not rename file: %s", old_filename);
         free(new_filename);
@@ -88,6 +114,7 @@ void process_directory(DIR *dir, size_t max_fname_len) {
 int main(const int argc, const char *argv[]) {
     char starting_path[PATH_MAX];
     char current_dir[PATH_MAX];
+    char *filename_prefix = NULL;
     int idx, args_processed;
     if (argc < 2) {
         usage(stderr, argv[0]);
@@ -122,7 +149,7 @@ int main(const int argc, const char *argv[]) {
             "can't access directory '%s'", current_dir
         );
 
-        process_directory(dir, new_filename_len);
+        process_directory(filename_prefix, dir, new_filename_len);
         closedir(dir);
         /* restore starting path */
         chdir(starting_path);
