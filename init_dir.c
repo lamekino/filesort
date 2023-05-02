a879e1063ed93a32bac4ad9eb18cf1f6ab04bf3a
lamekino <zoey.j.mcbride@gmail.com>
2023-05-01 21:14:42 -0400

    use realpath as malloc instead of staticly sized buffer

--- init_dir.c
+++ init_dir.c
@@ -12,9 +12,9 @@
 
 int init_dir(const char *relative_path, DIR **dir_p) {
     size_t new_filename_len = 0;
-    char full_path[PATH_MAX];
+    char *full_path = NULL;
 
-    realpath(relative_path, full_path);
+    full_path = realpath(relative_path, full_path);
     EXIT_WHEN(full_path == NULL,
         "could not alloc path '%s'", relative_path
     );
@@ -32,6 +32,7 @@ int init_dir(const char *relative_path, DIR **dir_p) {
         "can't access directory '%s'", full_path
     );
 
+    free(full_path);
     return new_filename_len;
 }
 
