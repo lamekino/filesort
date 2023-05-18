f4c92e4e194050e69d895ca31f5d6287a9224a94
lamekino <zoey.j.mcbride@gmail.com>
2023-05-17 20:33:30 -0400

    remove nonsense, handle errors

--- init_dir.c
+++ init_dir.c
@@ -1,3 +1,5 @@
+#include "init_dir.h"
+
 #include "error_handling.h"
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,22 +11,20 @@ int init_dir(const char *relative_path, DIR **dir_p) {
     char *full_path = NULL;
 
     full_path = realpath(relative_path, full_path);
-    EXIT_WHEN(full_path == NULL,
-        "could not alloc path '%s'", relative_path
-    );
+    if (full_path == NULL) return -1;
 
     *dir_p = opendir(full_path);
-
-    EXIT_WHEN(*dir_p == NULL,
-        "could not open directory '%s'", full_path
-    );
-    EXIT_WHEN(chdir(full_path) != 0,
-        "can't access directory '%s'", full_path
-    );
+    if (*dir_p == NULL) goto FREE_PATH;
+    if (chdir(full_path) != 0) goto CLOSE_DIR;
 
     new_filename_len = PATH_MAX - strnlen(full_path, PATH_MAX);
 
     free(full_path);
     return new_filename_len;
-}
 
+CLOSE_DIR:
+    closedir(*dir_p);
+FREE_PATH:
+    free(full_path);
+    return -1;
+}
