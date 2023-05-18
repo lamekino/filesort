2bd80cc65aacf372eef18250ef559228ab38b1c2
lamekino <zoey.j.mcbride@gmail.com>
2023-05-17 20:33:57 -0400

    use status_t, move applying changes to own function

--- main.c
+++ main.c
@@ -6,12 +6,34 @@
 #include "error_handling.h"
 #include "user_settings.h"
 #include "init_dir.h"
-#include "usage.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
+
+static status_t apply_on_dir(const struct user_settings settings,
+                             const int number_of_files,
+                             const char *starting_path,
+                             char **file_list) {
+    status_t status = STATUS_OK;
+    int idx;
+
+    for (idx = 0; !HAS_ERROR(status) && idx < number_of_files; idx++) {
+        DIR *dir = NULL;
+        int new_filename_len = init_dir(file_list[idx], &dir);
+        if (new_filename_len >= FNAME_MIN) {
+            status = process_directory(&settings, dir, new_filename_len);
+        }
+        else {
+            status = STATUS_ERR("error in opening file <placeholder>");
+        }
+        closedir(dir);
+        chdir(starting_path);
+    }
+
+    return status;
+}
 
 int main(int argc, char *argv[]) {
     struct user_settings settings = {0};
@@ -19,6 +41,7 @@ int main(int argc, char *argv[]) {
     int idx;
     char **files_to_process = NULL;
     int number_of_files = 0;
+    status_t outcome = STATUS_OK;
 
     if (argc < 2) {
         fprintf(stderr, "%s requires at least one argument\n", argv[0]);
@@ -31,19 +54,12 @@ int main(int argc, char *argv[]) {
     );
 
     number_of_files = read_args(&files_to_process, &settings, argc, argv);
-    for (idx = 0; idx < number_of_files; idx++) {
-        DIR *dir = NULL;
-        int new_filename_len = init_dir(files_to_process[idx], &dir);
-        EXIT_WHEN(new_filename_len < FNAME_MIN,
-            "path '%s' too long to rename file", files_to_process[idx]
-        );
-
-        process_directory(&settings, dir, new_filename_len);
-        closedir(dir);
-        chdir(starting_path);
-    }
-
+    outcome = apply_on_dir(settings, number_of_files, starting_path, files_to_process);
     free(files_to_process);
+
+    EXIT_WHEN(HAS_ERROR(outcome),
+        "%s", outcome.description
+    );
 
     return EXIT_SUCCESS;
 }
