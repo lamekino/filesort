787cb508905c5b15f29a53ea74ace90f5f91f5f1
lamekino <zoey.j.mcbride@gmail.com>
2023-12-30 15:09:54 -0500

    added ASSERT

--- src/util/error_handling.h
+++ src/util/error_handling.h
@@ -1,9 +1,19 @@
 #ifndef __ERROR_HANDLING_H
 #define __ERROR_HANDLING_H
+
+#ifdef DEBUG
+#include <assert.h>
+#define ASSERT(p) assert((p))
+#else
+#define ASSERT(_)
+#endif /* DEBUG */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+
+#define END_ENUM(name) name##_END
 
 typedef union {
     enum {
@@ -11,6 +21,7 @@ typedef union {
         SKIP,
         FAILED,
         FATAL_NO_MEM,
+        END_ENUM(status_t)
     } id;
     char *description;
 } status_t;
@@ -23,7 +34,11 @@ typedef union {
             s.id = FATAL_NO_MEM; \
             break; \
         }; \
-        snprintf(buf, STATUS_ERR_BUF_SIZE, msgs); \
+        if (snprintf(buf, STATUS_ERR_BUF_SIZE, msgs) < 0) { \
+            free(buf); \
+            s.id = FATAL_NO_MEM; \
+            break; \
+        } \
         s.description = buf; \
     } while (0)
 
@@ -31,7 +46,7 @@ typedef union {
 #define STATUS_SKIP   ((status_t) { .id = SKIP })
 #define STATUS_FAILED ((status_t) { .id = FAILED })
 
-#define HAS_ERROR(s)     ((s).id >= FATAL_NO_MEM)
+#define HAS_ERROR(s)     ((s).id >= END_ENUM(status_t))
 #define IS_FATAL_ERR(s)  ((s).id == FATAL_NO_MEM)
 #define HAS_FAILED(s)    ((s).id == FAILED)
 #define IS_NORMAL(s)     ((s).id == NORMAL)
