a95774bf98dc69d190a792d73972fbed7cef4f70
lamekino <zoey.j.mcbride@gmail.com>
2024-01-03 20:29:31 -0500

    check fgets return value

--- src/transform/confirm_rename.c
+++ src/transform/confirm_rename.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int confirm_rename(const char *src, const char *dest) {
     char user_input[64];
 
     printf("rename '%s' to '%s'? [y/N] ", src, dest);
-    fgets(user_input, 64, stdin);
-    if (user_input[0] == 'y' || user_input[0] == 'Y') {
+    if (fgets(user_input, 64, stdin) == NULL) {
+        printf("invalid input, skipping...");
+        return 0;
+    }
+
+    if (tolower(user_input[0]) == 'y') {
         printf("renamed '%s' ->'%s'\n", src, dest);
         return rename(src, dest);
     }
