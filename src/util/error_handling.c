f3ff3fd4e6f34c644a8806376fd706832b94867d
lamekino <zoey.j.mcbride@gmail.com>
2024-01-03 20:00:11 -0500

    Deprecate CREATE_STATUS_ERR to use function

---
+++ src/util/error_handling.c
@@ -0,0 +1,28 @@
+#include "util/error_handling.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+status_t create_status_err(const char *fmt, ...) {
+    va_list args;
+    char *dup = NULL;
+    char buf[STATUS_ERR_BUF_SIZE];
+
+    va_start(args, fmt);
+
+    if (vsnprintf(buf, STATUS_ERR_BUF_SIZE, fmt, args) < 0) {
+        return STATUS_NO_MEM;
+    }
+
+    dup = strndup(buf, STATUS_ERR_BUF_SIZE);
+    if (dup == NULL) {
+        return STATUS_NO_MEM;
+    }
+
+    va_end(args);
+    return (status_t) {
+        .description = dup
+    };
+}
--- src/util/error_handling.h
+++ src/util/error_handling.h
@@ -1,17 +1,14 @@
 #ifndef __ERROR_HANDLING_H
 #define __ERROR_HANDLING_H
 
+#define STATIC_ASSERT(COND,MSG) typedef char static_assertion_##MSG[(COND)?1:-1]
+
 #ifdef DEBUG
 #include <assert.h>
 #define ASSERT(p) assert((p))
 #else
 #define ASSERT(_)
 #endif /* DEBUG */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
 
 #define END_ENUM(name) name##_END
 
@@ -27,24 +24,11 @@ typedef union {
 } status_t;
 
 #define STATUS_ERR_BUF_SIZE 256
-#define CREATE_STATUS_ERR(s, msgs...) \
-    do { \
-        char *buf = malloc(STATUS_ERR_BUF_SIZE); \
-        if (buf == NULL) { \
-            s.id = FATAL_NO_MEM; \
-            break; \
-        }; \
-        if (snprintf(buf, STATUS_ERR_BUF_SIZE, msgs) < 0) { \
-            free(buf); \
-            s.id = FATAL_NO_MEM; \
-            break; \
-        } \
-        s.description = buf; \
-    } while (0)
 
 #define STATUS_NORMAL ((status_t) {0})
 #define STATUS_SKIP   ((status_t) { .id = SKIP })
 #define STATUS_FAILED ((status_t) { .id = FAILED })
+#define STATUS_NO_MEM ((status_t) { .id = FATAL_NO_MEM })
 
 #define HAS_ERROR(s)     ((s).id >= END_ENUM(status_t))
 #define IS_FATAL_ERR(s)  ((s).id == FATAL_NO_MEM)
@@ -52,23 +36,6 @@ typedef union {
 #define IS_NORMAL(s)     ((s).id == NORMAL)
 #define IS_SKIPPED(s)    ((s).id == SKIP)
 
-#define UNIMPLEMENTED \
-    do { \
-        fprintf(stderr, "%s: '%s()', line %d not implemented, exiting\n", \
-                __FILE__, __FUNCTION__, __LINE__); \
-        exit(EXIT_FAILURE); \
-    } while (0)
-
-#define WARNING(msgs...) \
-    do { \
-        int errsv = errno; \
-        fprintf(stderr, "ERROR: %s[%d]: ", __FILE__, __LINE__); \
-        fprintf(stderr, msgs); \
-        if (errsv > 0) { \
-            fprintf(stderr, "\n\t-> errno is '%s' (%d)", \
-                    strerror(errsv), errsv); \
-        } \
-        fprintf(stderr, "\n"); \
-    } while (0);
+status_t create_status_err(const char *fmt, ...);
 
 #endif
