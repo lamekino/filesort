ff598a1754605b91cdfb3220293ea1213833063c
lamekino <zoey.j.mcbride@gmail.com>
2024-01-05 20:32:54 -0500

    fixed bug on access() check

--- src/process_file/get_new_filename.c
+++ src/process_file/get_new_filename.c
@@ -1,14 +1,16 @@
 #include "process_file/get_new_filename.h"
 
 #include <stdbool.h>
+#include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
-#include "error_handling.h"
 #include "str_append.h"
 #include "settings.h"
 #include "process_file/file_info.h"
 
-#define MAX_DUPLICATES 128u
+/* FIXME: this is not thread safe... */
+#define file_exists(filename) (access(filename, F_OK) != -1)
 
 static size_t get_possible_filename(char *buffer,
                                     const size_t max_len,
@@ -16,13 +18,13 @@ static size_t get_possible_filename(char *buffer,
                                     const struct file_info *info) {
     size_t offset = 0;
 
-    #define BUFFER_APPEND(...) offset += \
-        str_append(buffer, offset, max_len, __VA_ARGS__)
+    #define BUFFER_APPEND(...) \
+        offset += str_append(buffer, offset, max_len, __VA_ARGS__)
 
     /*
      * Do the pre- timestamp parts
      */
-    if (settings->prefix != NULL) {
+    if (settings->prefix) {
         BUFFER_APPEND("%s", settings->prefix);
     }
     /*
@@ -33,20 +35,17 @@ static size_t get_possible_filename(char *buffer,
     /*
      * do the post- timestamp parts
      */
-    if (info->num_duplicates > 0) {
-        BUFFER_APPEND(".%d", info->num_duplicates);
+    if (info->duplicates) {
+        BUFFER_APPEND(".%d", info->duplicates);
     }
-
-    if (settings->suffix != NULL) {
+    if (settings->suffix) {
         BUFFER_APPEND("%s", settings->suffix);
     }
-
-    if (info->extension != NULL) {
+    if (info->extension) {
         BUFFER_APPEND("%s", info->extension);
     }
-
-    if (settings->appendix != NULL) {
-        BUFFER_APPEND("%s", settings->suffix);
+    if (settings->appendix) {
+        BUFFER_APPEND("%s", settings->appendix);
     }
 
     #undef BUFFER_APPEND
@@ -54,21 +53,21 @@ static size_t get_possible_filename(char *buffer,
     return offset;
 }
 
-size_t get_new_filename(char *buffer,
+size_t get_new_filename(char *dest,
                         const size_t max_len,
                         const settings_t *settings,
                         struct file_info *info) {
     size_t name_len = 0;
-    bool file_exists = false;
+    char buffer[128];
 
-    while (file_exists) {
-        name_len = get_possible_filename(buffer, max_len, settings, info);
+    while (true) {
+        name_len =
+            get_possible_filename(buffer, sizeof(buffer), settings, info);
 
-        info->num_duplicates++;
-        if (info->num_duplicates == MAX_DUPLICATES) {
-            ASSERT(0 && "TODO: implement errors here");
-        }
+        if (!file_exists(buffer)) break;
+        if (++info->duplicates >= MAX_DUPLICATES) break;
     }
 
+    strncpy(dest, buffer, max_len);
     return name_len;
 }
