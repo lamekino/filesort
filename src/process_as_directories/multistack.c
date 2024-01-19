077d1fca25fd5a7ab6201a7643f7b4d6c292556d
lamekino <zoey.j.mcbride@gmail.com>
2024-01-19 04:40:26 -0500

    strdup name in push_name, refactor

--- src/process_as_directories/multistack.c
+++ src/process_as_directories/multistack.c
@@ -12,9 +12,9 @@
 
 static void *
 push(void *p, size_t *length, size_t *capacity, size_t elem_size) {
-    size_t cur_cap = *capacity;
-    size_t new_len = *length + 1;
-    size_t new_cap = 2 * cur_cap + 1;
+    const size_t cur_cap = *capacity;
+    const size_t new_len = *length + 1;
+    const size_t new_cap = 2 * cur_cap + 1;
     void *ys = p;
 
     if (new_len > cur_cap) {
@@ -30,49 +30,71 @@ push(void *p, size_t *length, size_t *capacity, size_t elem_size) {
 }
 
 
-static struct stack
-init_stack(char *name, char **members, size_t capacity) {
-    return (struct stack) {
-        .name = name,
-        .members = members,
-        .capacity = capacity,
-        .count = 0
-    };
-}
+static struct stack *
+push_stack(struct multistack *ms, char *name, char **members,
+        size_t stack_cap) {
+    const size_t idx = ms->len - 1;
+    struct stack *base =
+        push(ms->base, &ms->len, &ms->capacity, SIZE_AT(ms->base));
 
-struct stack *
-push_name(struct multistack *ms, char *name) {
-    struct stack init;
-    struct stack *base = ms->base;
-    const size_t idx = ms->len;
-    char **members = NULL;
-
-    members = calloc(INITIAL_STACK_CAPACITY, sizeof(char **));
-    base = push(ms->base, &ms->len, &ms->capacity, SIZE_AT(ms->base));
-    if (!members || !base) {
+    if (!base) {
         return NULL;
     }
 
     ms->base = base;
-    ms->base[idx] = init_stack(name, members, INITIAL_STACK_CAPACITY);
+    ms->base[idx] =
+        (struct stack) {
+            .name = name,
+            .members = members,
+            .capacity = stack_cap,
+            .count = 0
+        };
+
     return &ms->base[idx];
 }
 
-char **
-push_member(struct multistack *ms, char *member) {
-    struct stack *cur = &ms->base[ms->len - 1];
-    const size_t idx = cur->count;
-    char *dup = strdup(member);
+struct stack *
+push_name(struct multistack *ms, char *name) {
+    char **members = calloc(INITIAL_STACK_CAPACITY, sizeof(char **));
+    char *dup = strdup(name);
+    struct stack *ys = push_stack(ms, dup, members, INITIAL_STACK_CAPACITY);
+
+    if (!ys || !members || !dup) {
+        free(members);
+        free(dup);
+        return NULL;
+    }
+
+    return ys;
+}
+
+static char **
+push_string(struct stack *xs, char *name) {
+    const size_t idx = xs->count - 1;
     char **members =
-        push(cur->members, &cur->count, &cur->capacity, SIZE_AT(cur->members));
+        push(xs->members, &xs->count, &xs->capacity, SIZE_AT(xs->members));
 
     if (!members) {
         return NULL;
     }
 
-    cur->members = members;
-    cur->members[idx] = strdup(member);
-    return &cur->members[idx];
+    xs->members = members;
+    xs->members[idx] = name;
+    return &xs->members[idx];
+}
+
+char **
+push_member(struct multistack *ms, char *member_name) {
+    struct stack *cur = &ms->base[ms->len - 1];
+    char *dup = strdup(member_name);
+    char **ys = push_string(cur, dup);
+
+    if (!ys || !dup) {
+        free(dup);
+        return NULL;
+    }
+
+    return ys;
 }
 
 struct stack *
@@ -110,6 +132,7 @@ cleanup_multistack(struct multistack *ms) {
         }
 
         free(cur->members);
+        free(cur->name);
         free(cur);
     }
 }
