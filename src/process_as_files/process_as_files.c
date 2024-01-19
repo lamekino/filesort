c28ccc2ffebf86f65ac42a947ef0cc8375790623
lamekino <zoey.j.mcbride@gmail.com>
2024-01-19 10:47:01 -0500

    stub

---
+++ src/process_as_files/process_as_files.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "process_as_files/process_as_files.h"
+#include "types/strings.h"
+#include "types/error.h"
+
+union error
+process_as_files(const struct settings *settings, char **files, size_t len) {
+    size_t i;
+
+    for (i = 0; i < len; i++) {
+    }
+
+    UNIMPLEMENTED;
+}
